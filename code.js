024d11206ec61edfa8dc651f6ef07f97b43f044b
andrew-semenov <asemenov@remotehub.com>
2025-04-11 23:28:15 +0300

    test

--- code.js
+++ code.js
@@ -1,2 +1,26 @@
 const res  = "commit!";
 console.log("first ", res);
+
+let testmytest = document.getElementById("testmytest")
+
+// testmytest.style="border: 4px solid blue"
+
+const textnode = document.createTextNode("Water2");
+const node = document.createElement("li");
+
+node.appendChild(textnode)
+testmytest.appendChild(node)
+testmytest.append(node)
+
+
+// // Create an "li" node:
+// const node = document.createElement("li");
+
+// // Create a text node:
+// // const textnode = document.createTextNode("Water");
+
+// // Append the text node to the "li" node:
+// node.appendChild(textnode);
+
+// // Append the "li" node to the list:
+// document.getElementById("myList").appendChild(node);
